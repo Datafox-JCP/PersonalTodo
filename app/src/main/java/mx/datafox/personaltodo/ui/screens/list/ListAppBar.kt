40731626a0abc9569a7300a4021af4f573ef6ff6
Datafox-JCP <datafox@outlook.com>
2023-03-26 12:53:44 -0600

    - TopBarActions UI implementation

--- app/src/main/java/mx/datafox/personaltodo/ui/screens/list/ListAppBar.kt
+++ app/src/main/java/mx/datafox/personaltodo/ui/screens/list/ListAppBar.kt
@@ -1,32 +1,165 @@
 package mx.datafox.personaltodo.ui.screens.list
 
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.TopAppBar
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import mx.datafox.personaltodo.R
+import mx.datafox.personaltodo.components.PriorityItem
+import mx.datafox.personaltodo.data.models.Priority
+import mx.datafox.personaltodo.ui.theme.LARGE_PADDING
 import mx.datafox.personaltodo.ui.theme.topAppBarBackgroundColor
 import mx.datafox.personaltodo.ui.theme.topAppBarContentColor
+import mx.datafox.personaltodo.ui.theme.Typography
 
 @Composable
 fun ListAppBar() {
-    DefaultListAppbar()
+    DefaultListAppbar(
+        onSearchClicked = {},
+        onSortClicked = {},
+        onDeleteClicked = {}
+    )
 }
 
 @Composable
-fun DefaultListAppbar() {
+fun DefaultListAppbar(
+    onSearchClicked: () -> Unit,
+    onSortClicked: (Priority) -> Unit,
+    onDeleteClicked: () -> Unit
+) {
     TopAppBar(
         title = {
             Text(
                 "Tasks",
                 color = MaterialTheme.colors.topAppBarContentColor)
         },
+        actions = {
+            ListAppBarActions(
+                onSearchClicked = onSearchClicked,
+                onSortClicked =  onSortClicked,
+                onDeleteClicked = onDeleteClicked
+            )
+        },
         backgroundColor = MaterialTheme.colors.topAppBarBackgroundColor
     )
 }
 
 @Composable
+fun ListAppBarActions(
+    onSearchClicked: () -> Unit,
+    onSortClicked: (Priority) -> Unit,
+    onDeleteClicked: () -> Unit
+) {
+    SearchAction(onSearchClicked = onSearchClicked)
+    SortAction(onSortClicked = onSortClicked)
+    DeleteAllAction(onDeleteClicked = onDeleteClicked)
+}
+
+@Composable
+fun SearchAction(
+    onSearchClicked: () -> Unit
+) {
+    IconButton(onClick = { onSearchClicked() }) {
+        Icon(
+            imageVector = Icons.Filled.Search,
+            contentDescription = stringResource(R.string.search_tasks),
+            tint = MaterialTheme.colors.topAppBarContentColor
+        )
+    }
+}
+
+@Composable
+fun SortAction(
+    onSortClicked: (Priority) -> Unit
+) {
+    var expanded by remember { mutableStateOf(false) }
+
+    IconButton(
+        onClick = { expanded = true }
+    ) {
+        Icon(
+            painter = painterResource(id = R.drawable.baseline_filter_list),
+            contentDescription = stringResource(R.string.sort_tasks),
+            tint = MaterialTheme.colors.topAppBarContentColor
+        )
+        DropdownMenu(
+            expanded = expanded,
+            onDismissRequest = { expanded = false }
+        ) {
+            DropdownMenuItem(
+                onClick = {
+                    expanded = false
+                    onSortClicked(Priority.LOW)
+                }
+            ) {
+                PriorityItem(priority = Priority.LOW)
+            }
+            DropdownMenuItem(
+                onClick = {
+                    expanded = false
+                    onSortClicked(Priority.HIGH)
+                }
+            ) {
+                PriorityItem(priority = Priority.HIGH)
+            }
+            DropdownMenuItem(
+                onClick = {
+                    expanded = false
+                    onSortClicked(Priority.NONE)
+                }
+            ) {
+                PriorityItem(priority = Priority.NONE)
+            }
+        }
+    }
+}
+
+@Composable
+fun DeleteAllAction(
+    onDeleteClicked: () -> Unit
+) {
+    var expanded by remember { mutableStateOf(false) }
+
+    IconButton(
+        onClick = { expanded = true }
+    ) {
+        Icon(
+            painter = painterResource(id = R.drawable.baseline_delete),
+            contentDescription = stringResource(R.string.delete_all),
+            tint = MaterialTheme.colors.topAppBarContentColor
+        )
+        DropdownMenu(
+            expanded = expanded,
+            onDismissRequest = { expanded = false }
+        ) {
+            DropdownMenuItem(
+                onClick = {
+                    expanded = false
+                    onDeleteClicked()
+                }
+            ) {
+                Text(
+                    modifier = Modifier
+                        .padding(start = LARGE_PADDING),
+                    text = stringResource(R.string.delete_all),
+                    style = Typography.subtitle2
+                )
+            }
+        }
+    }
+}
+
+@Composable
 @Preview
 fun DefaultListAppbarPreview() {
-    DefaultListAppbar()
+    DefaultListAppbar(
+        onSearchClicked = {},
+        onSortClicked = {},
+        onDeleteClicked = {}
+    )
 }
--- app/src/main/java/mx/datafox/personaltodo/ui/screens/list/ListScreen.kt
+++ app/src/main/java/mx/datafox/personaltodo/ui/screens/list/ListScreen.kt
@@ -3,6 +3,7 @@ package mx.datafox.personaltodo.ui.screens.list
 import android.annotation.SuppressLint
 import androidx.compose.material.FloatingActionButton
 import androidx.compose.material.Icon
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Scaffold
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
@@ -11,11 +12,12 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import mx.datafox.personaltodo.R
+import mx.datafox.personaltodo.ui.theme.fabBackgroundColor
 
 @SuppressLint("UnusedMaterialScaffoldPaddingParameter")
 @Composable
 fun ListScreen(
-    navigateToTaskScreen: (Int) -> Unit
+    navigateToTaskScreen: (taskId: Int) -> Unit
 ) {
     Scaffold(
         topBar = {
@@ -30,12 +32,14 @@ fun ListScreen(
 
 @Composable
 fun ListFab(
-    onFavClicked: (Int) -> Unit
+    onFavClicked: (taskId: Int) -> Unit
 ) {
     FloatingActionButton(
         onClick = {
             onFavClicked(-1)
-        }) {
+        },
+        backgroundColor = MaterialTheme.colors.fabBackgroundColor
+    ) {
         Icon(
             imageVector = Icons.Filled.Add,
             contentDescription = stringResource(R.string.add_task),
